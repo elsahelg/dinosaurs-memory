64f551155010bdba872f3162c006c18b008cc3a0
Elsa Helg <elsahelg@gmail.com>
2022-11-26 11:45:46 +0000

    Add eventlistener for COM content load, linked images from folder to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,53 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cardsList = [
+    {
+        name: "acanthopholis",
+        image: ".assets/images/acanthopholis.jpg"    
+    },
+    {
+        name: "acanthopholis",
+        image: ".assets/images/acanthopholis.jpg"    
+    },
+    {
+        name: "parasaurolophus",
+        image: ".assets/images/parasaurolophus.jpg"    
+    },
+    {
+        name: "parasaurolophus",
+        image: ".assets/images/parasaurolophus.jpg"    
+    },
+    {
+        name: "pterodactyl",
+        image: ".assets/images/pterodactyl.jpg"    
+    },
+    {
+        name: "pterodactyl",
+        image: ".assets/images/pterodactyl.jpg"    
+    },
+    {
+        name: "stegosaurus",
+        image: ".assets/images/stegosaurus.jpg"    
+    },
+    {
+        name: "stegosaurus",
+        image: ".assets/images/stegosaurus.jpg"    
+    },
+    {
+        name: "triceratops",
+        image: ".assets/images/triceratops.jpg"
+    },
+    {
+        name: "triceratops",
+        image: ".assets/images/triceratops.jpg"
+    },
+    {
+        name: "tyrannosaurusrex",
+        image: ".assets/images/tyrannosaurusrex.jpg"
+    }
+    ];
+})
 
 // shuffle cards, initiate memory board
 
